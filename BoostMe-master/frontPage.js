57473731c5e93ae77ba42e3b602af13122e887b9
JordanPag <JordanPag@users.noreply.github.com>
2018-10-14 15:10:42 -0400

    Update frontPage.js

--- BoostMe-master/frontPage.js
+++ BoostMe-master/frontPage.js
@@ -10,17 +10,22 @@ function login() {
   window.location.replace("login.html");
 }
 
-function continuousShift1() {
+function continuousShiftBeginning() {
   window.location.replace("frontPage.html#slider-image-1");
-  setTimeout(function() { continuousShift2() }, 7000);
+  setTimeout(function() { continuousShift() }, 7000);
 }
-function continuousShift2() {
-  window.location.replace("frontPage.html#slider-image-2");
-  setTimeout(function() { continuousShift3() }, 7000);
-}
-function continuousShift3() {
-  window.location.replace("frontPage.html#slider-image-3");
-  setTimeout(function() { continuousShift1() }, 7000);
+function continuousShift() {
+  var url = window.location.href;
+  var slide = url[(url.length-1)];
+  if(slide==1) {
+    window.location.replace("frontPage.html#slider-image-2");
+  } else if(slide==2) {
+    window.location.replace("frontPage.html#slider-image-3");
+  } else if(slide==3) {
+    window.location.replace("frontPage.html#slider-image-1");
+  }
+  setTimeout(function() { continuousShift() }, 7000);
 }
 
-window.onload = continuousShift1;
+
+window.onload = continuousShiftBeginning;
