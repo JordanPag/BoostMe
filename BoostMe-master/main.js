34fc93f1fc35a1888e64c41d0bd12e226489a477
JordanPag <JordanPag@users.noreply.github.com>
2018-10-19 19:55:20 -0400

    Add files via upload

---
+++ BoostMe-master/main.js
@@ -0,0 +1,15 @@
+function getStarted() {
+  window.location.replace("signup.html");
+}
+
+function learnMore() {
+  window.location.replace("learnMore.html");
+}
+
+function login() {
+  window.location.replace("login.html");
+}
+
+function frontPage() {
+  window.location.replace("frontPage.html");
+}
