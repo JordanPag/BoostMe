4bde6e1eb2425dc62002bd361b51df4637124a47
JordanPag <JordanPag@users.noreply.github.com>
2018-11-02 15:39:54 -0400

    Updated backend

--- BoostMe-master/signup.js
+++ BoostMe-master/signup.js
@@ -1,15 +1,24 @@
+CB.CloudApp.init('ayyuhziequij', 'e592ce9a-b29b-444c-b5b2-138f4572644f');
+
 function signUp() {
   //Add info to database and redirect to homepage
   var user = new CB.CloudUser();
+  var username = $("#username").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var genre = $("#genre").val();
+  var musiclink = $("#musiclink").val();
   user.set('username', username);
+  user.set('email', email);
   user.set('password', password);
   user.set('musicGenre', genre);
-  user.signUp({
-    success: function(user) {
+  user.set('musicLink', musiclink);
+  //user.signUp({
+    //success: function(user) {
       //Registration successfull
-    },
-    error: function(error) {
+    //},
+    //error: function(error) {
       //Error in user registration.
-    }
-  });
+    //}
+  //});
 }
