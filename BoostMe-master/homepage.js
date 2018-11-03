9e30286ff96210f6916548b68572a246c6e1e87e
JordanPag <JordanPag@users.noreply.github.com>
2018-11-02 21:00:01 -0400

    Some changes

--- BoostMe-master/homepage.js
+++ BoostMe-master/homepage.js
@@ -1,16 +1,28 @@
 CB.CloudApp.init('ayyuhziequij', 'e592ce9a-b29b-444c-b5b2-138f4572644f');
 var username = CB.CloudUser.current.document.username;
 
-function changeUsername() {
+function personalizeWebsite() {
   console.log(username);
   $("#username").html(username);
+  var query = new CB.CloudQuery("User");
+  query.equalTo('group', 1);
+  query.find({
+    success: function(list){
+      console.log(list);
+    },
+    error: function(err){
+      console.log(err);
+    }
+  })
 }
-window.onload = changeUsername;
+window.onload = personalizeWebsite;
+
 CB.CloudUser.getCurrentUser({
   success: function(user) {
     console.log(user);
   },
   error: function(error) {
    console.log(error);
+   window.location.replace("login.html");
   }
 });
